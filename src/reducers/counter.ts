d209e154d8c4f905f7ba9a9140d33a3965738b4e
和田 充弘 <benzoh.wada@gmail.com>
2020-06-20 21:37:08 +0900

    ## Reducerのベース作成と初期状態の設計

---
+++ src/reducers/counter.ts
@@ -0,0 +1,14 @@
+export function createInitialState() {
+  return {
+    current: 0,
+  };
+}
+
+export type State = ReturnType<typeof createInitialState>;
+
+export default function reducer(state = createInitialState(), action: Action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
